3f137406f52777f460ee0482422438ad1eb469a5
ly-cardozo <eliane.gomes.cardozo@gmail.com>
2020-11-05 12:27:59 -0300

    Alteração no nome de algumas id no arquivo index.html; adicionada funções no arquivo index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
+//  Segundo o vídeo do Daniel: ((códigoDaLetraASC - código1letra + deslocamento) %(módulo, que é o giro que vai dar) tamanhoAlfabeto) + código1letra
+//                             ((códigoDaLetraASC -      65      + deslocamento) %(módulo, que é o giro que vai dar)      26        ) +      65
+//  ["string".charCodeAt(número)] => ["A".charCodeAt(0)=65] essa função serve para saber o código da string em determinada posição. Nesse exemplo, código ASC
+//  Alguns métodos de acesso DOM => são formas do JS entender ao quê estamos referenciando. Tudo o que tenha document se refere ao arquivo html [ATENÇÃO COLOCAR ISSO NO ARQUIVO INDEX.JS]
+//    getElementById(id) => [document.getElementById]
+//    getElementsByClassName(className)
+//    getElementsByTagName(tagName)
+//    querySelector(cssSelector)               
+//    querySelectorAll(cssSelector)
+//    addEventListener => ligar um evento de um objeto a uma função
+//    innerHTML => troca o conteúdo de um elemento: [document.body.innerHTML = "";] (troca o conteúdo de body por uma string vazia)
+
 const cipher = {
-  // ...
-};
+  encode(offset, originalMsg) {
+    offset = offset % 26;
+    originalMsg = originalMsg.toUpperCase()
+
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let key = alphabet.charAt(offset);
+    let splitAlphabet = alphabet.split(key);
+
+    let newAlphabet = key + splitAlphabet[1] + splitAlphabet [0]
+    let newMsg = '';
+    for(let i=0; i < originalMsg.lenght; i++){
+      if(originalMsg.charAt(i) == ''){
+        newMsg+= '';
+    }else if(originalMsg.charAt(i) == ','){
+      newMsg+= ',';
+    }
+      else{
+        let number = alphabet.indexOf(originalMsg.charAt(i));
+        newMsg += newAlphabet[number];
+      }
+  }
+  return newMsg;
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//Alguns métodos de acesso DOM => são formas do JS entender ao quê estamos referenciando. Tudo o que tenha document se refere ao arquivo html
+//  => addEventListener() => ligar um evento de um objeto a uma função
+//  => innerHTML() => troca o conteúdo de um elemento: [document.body.innerHTML = "";] (troca o conteúdo de body por uma string vazia)
+//  => toUpperCase() => retorna valor da string original convertida em maiúscula
+
+//Criei uma constante e chamei cada um dos meus id do arquivo index.html
+//Aqui eu poderia usar o getElementById(id) porque usei uma id para meus textos
+//mas preferi usar o querySelector(cssSelector) por ser mais rápido. Posso usar ('#') para id e ('.') para class
+//Exemplo da outra forma: const originalMsg = document.getElementById('original-Msg');
+const originalMsg = document.querySelector('#original-Msg');
+const offsetNumber = document.querySelector('#offset-Number');
+const encryptButton = document.querySelector('#encrypt-Button');
+const decryptButton = document.querySelector('#decrypt-Button');
+const cleartButton = document.querySelector('#clear-Button');
+const translatedMsg = document.querySelector('#translated-Msg');
+
+
+encryptButton.addEventListener('click', function () {
+    const textOriginal = string(originalMsg.value).toUpperCase();
+    const offset = Number(offsetNumber.value);
+    const encryptMsg = cipher.encode(offset, textOriginal);
+    translatedMsg.value = encryptMsg;
+})
+
+decryptButton.addEventListener('click', function () {
+    const textOriginal = string(originalMsg.value).toUpperCase();
+    const offset = Number(offsetNumber.value);
+    const decryptMsg = cipher.encode(offset, textOriginal);
+    translatedMsg.value = decryptMsg;
+})
+
+cleartButton.addEventListener('click', function () {
+    originalMsg.value = '';
+    offsetNumber.value = '';
+    translatedMsg.value = '';
+})
