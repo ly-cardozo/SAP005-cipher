53d057865990299d9fb67f98bdbb8011269e5a10
ly-cardozo <eliane.gomes.cardozo@gmail.com>
2020-11-05 19:09:47 -0300

    Exclusão da primeira imagem de fundo; alterações nos arquivos .js e .html (inclusão disabled em textarea)

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,8 @@
 //  Segundo o vídeo do Daniel: ((códigoDaLetraASC - código1letra + deslocamento) %(módulo, que é o giro que vai dar) tamanhoAlfabeto) + código1letra
 //                             ((códigoDaLetraASC -      65      + deslocamento) %(módulo, que é o giro que vai dar)      26        ) +      65
 //  ["string".charCodeAt(número)] => ["A".charCodeAt(0)=65] essa função serve para saber o código da string em determinada posição. Nesse exemplo, código ASC
-//  Alguns métodos de acesso DOM => são formas do JS entender ao quê estamos referenciando. Tudo o que tenha document se refere ao arquivo html [ATENÇÃO COLOCAR ISSO NO ARQUIVO INDEX.JS]
-//    getElementById(id) => [document.getElementById]
-//    getElementsByClassName(className)
-//    getElementsByTagName(tagName)
-//    querySelector(cssSelector)               
-//    querySelectorAll(cssSelector)
-//    addEventListener => ligar um evento de um objeto a uma função
-//    innerHTML => troca o conteúdo de um elemento: [document.body.innerHTML = "";] (troca o conteúdo de body por uma string vazia)
+
+ 
 
 const cipher = {
   encode(offset, originalMsg) {
