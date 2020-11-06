175e210532f5a6eaa972e806c82548b7d3151090
ly-cardozo <eliane.gomes.cardozo@gmail.com>
2020-11-06 18:34:57 -0300

    Últimas alterações nos arquivos .js, .html e .css

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,11 @@
-//  Segundo o vídeo do Daniel: ((códigoDaLetraASC - código1letra + deslocamento) %(módulo, que é o giro que vai dar) tamanhoAlfabeto) + código1letra
-//                             ((códigoDaLetraASC -      65      + deslocamento) %(módulo, que é o giro que vai dar)      26        ) +      65
-//  ["string".charCodeAt(número)] => ["A".charCodeAt(0)=65] essa função serve para saber o código da string em determinada posição. Nesse exemplo, código ASC
-//  "string".length => contém o comprimento da string
-//  "string".fromCharcode()
-//  for (let i=0; i<input.lenght; i++) +> vai "olhar" cada um dos caracteres o elemento
-//  originalMsg.charCodeAt(i) - 65 => identifica a letra do alfabeto
-//  (positionAlphabet + offsetNumber) % 26) + 65 => 
-
- 
-
 const cipher = {
 
   encode: function (offsetNumber, originalMsg) {
     originalMsg = originalMsg.replace('Ç', 'C');
-    originalMsg = originalMsg.replace('', '');
-    const offsetNumberError = !offsetNumber;
-    const originalMsgError = !originalMsg
     let enigmaDoNerd = "";
 
-    if (offsetNumberError || originalMsgError) {
-      throw new TypeError('User did not enter the data correctly', 'cipher.js', 19);
+    if (!offsetNumber || !originalMsg) {
+      throw new TypeError('User did not enter the data correctly', 'cipher.js', 8);
     } else {
       for (let i=0; i <originalMsg.length; i++) {
         const positionAlphabet = originalMsg.charCodeAt(i) - 65;
@@ -29,7 +15,6 @@ const cipher = {
         enigmaDoNerd += encryptLetter;
       }
       return enigmaDoNerd;
-      console.log(enigmaDoNerd)
     }
 
   },
@@ -38,10 +23,10 @@ const cipher = {
     let enigmaDoNerd = "";
 
     if (!offsetNumber || !originalMsg) {
-      throw new TypeError('User did not enter the data correctly', 'cipher.js', 19);
+      throw new TypeError('User did not enter the data correctly', 'cipher.js', 8);
     } else {
       for (let i = 0; i < originalMsg.length; i++) {
-        const positionAlphabetDecrypt = originalMsg.charCodeAt(i) - 65;
+        const positionAlphabetDecrypt = originalMsg.charCodeAt(i) + 65;
         const newAlphabetCharCode = ((positionAlphabetDecrypt - offsetNumber) % 26) + 65;
         const decryptLetter = String.fromCharCode(newAlphabetCharCode);
 
@@ -53,3 +38,13 @@ const cipher = {
 };
 
 export default cipher;
+
+//  Segundo o vídeo do Daniel: ((códigoDaLetraASC - código1letra + deslocamento) %(módulo, que é o giro que vai dar) tamanhoAlfabeto) + código1letra
+//                             ((códigoDaLetraASC -      65      + deslocamento) %(módulo, que é o giro que vai dar)      26        ) +      65
+//  ["string".charCodeAt(número)] => ["A".charCodeAt(0)=65] essa função serve para saber o código da string em determinada posição. Nesse exemplo, código ASC
+//  "string".length => contém o comprimento da string
+//  "string".charCodeAt() => passa de código  ASCII para 0 a 25
+//  string".fromCharCode() => pega esse código de 0 a 25 e retorna para ASCII 
+//  "string".replace => permite substituir uma sequência de caracteres por outro, no caso, substitui o Ç por C
+//  for (let i=0; i<input.lenght; i++) +> vai "olhar" cada um dos caracteres o elemento
+//  originalMsg.charCodeAt(i) - 65 => identifica a letra do alfabeto
